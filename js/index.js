a77a539167b74e1b96122275e2b784b97c445e2c
louisan9691 <louis.an9691@gmail.com>
2017-06-30 16:13:13 +1000

    Finished the layout for desktop. Working on mobile next

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $('.image .top').on('click', function(){
+    $(this).toggleClass('show');
+    $('.image .btm').toggleClass('show');
+  })
+
+  $('.image .btm').on('click', function(){
+    $(this).toggleClass('show');
+    $('.image .top').toggleClass('show');
+  })
+})
