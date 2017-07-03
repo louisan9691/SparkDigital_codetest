5b599f4ce7507f7cea5cdf2f68bdea98f683aa78
louisan9691 <louis.an9691@gmail.com>
2017-07-03 22:03:49 +1000

    Minor error

--- js/index.js
+++ js/index.js
@@ -2,10 +2,10 @@ $(document).ready(function() {
   $('.image .top').on('click', function(){
     $(this).toggleClass('show');
     $('.image .btm').toggleClass('show');
-  })
+  });
 
   $('.image .btm').on('click', function(){
     $(this).toggleClass('show');
     $('.image .top').toggleClass('show');
-  })
-})
+  });
+});
